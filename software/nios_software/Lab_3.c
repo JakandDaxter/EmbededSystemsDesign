9e135b717ac2dde5bcf65aec96a5a9f8041c304c
JakandDaxter <aliana124@gmail.com>
2019-02-15 03:44:39 -0500

    Update Lab_3.c

--- software/nios_software/Lab_3.c
+++ software/nios_software/Lab_3.c
@@ -34,16 +34,91 @@ volatile uint32 *LedPtr           	= (uint32 *) 0x11020;
 volatile uint32 *Hex0Ptr      	    = (uint32 *) 0x11030;
 volatile uint32 *KeyPtr       	    = (uint32 *) 0x11040;
 volatile uint32 *SwitchPtr        	= (uint32 *) 0x11050;
+//************************************************************************************************************************//
+//************************************************************************************************************************//
+//************************************************************************************************************************//
+// 1. Write a NIOS II C program that does the following:
+// a. Displays 0 on hex0
+// b. Checks to see if key1 is pushed (active low)
+// i. If SW0 is high, increments the value on hex0 ii. If SW0 is low, decrements the value on hex0
+// c. Do not increment or decrement the value on hex0 until key1 is released
+//************************************************************************************************************************//
+
+// int main(void)
+// // ****************************************************************************/
+// // /* Main Program   Part 1                                                    */
+// // /*   polling to increment and decrement                                    */
+// // ***************************************************************************
+// {
+//
+// 	int j = 0;
+// 	int startloop = 0;
+// //	Set up the board and variables
+// 	int current_Keyval,switch_val; //will store the value of what ever is being read
+// //	   what is stored in the bucket
+// 	char array_hexDisplay[] = {0x40, 0x79, 0x24, 0x30, 0x19, 0x12, 0x2, 0x78, 0x0, 0x18}; //array to hold the hex constants
+//
+// 	*Hex0Ptr = array_hexDisplay[0] ;
+// 	//current_Keyval = *KeyPtr; //reading the current value of the key
+//
+// /* 		if(current_Keyval == 15)
+// 			{startloop = 0;}
+// 		else{startloop=1;} */
+//
+// 		//while(startloop == 1){
+// 	while(1){
+// 	 for(int i = 0; i<10;i++)
+// 	 {
+// 		 *Hex0Ptr = array_hexDisplay[j] ;
+// 		 	 current_Keyval = *KeyPtr; //reading the current value of the key
+//
+// 		 	 if(current_Keyval == 13)
+// 		 	 {
+// 		 		 current_Keyval = *KeyPtr; //reading the current value of the key
+//
+// 		 		 if(current_Keyval == 15){
+//
+// 		 			 switch_val = *SwitchPtr; //reading the current value of the key
+//
+// 		 			 if(switch_val == 1){
+// 		 				 if(j == 10){j = 0;}
+// 						 else{j++;} //increment the bucket of the array
+// 		 	 	 	 }
+// 		 			 else{
+// 		 				 if(j == 0){j = 10; j--;}
+// 				 		j--;
+// 				 }
+//
+// 			 }
+// 		 }
+// 	 }
+//  		}
+//
+//     return (0);
+// }
+//************************************************************************************************************************//
+//************************************************************************************************************************//
+//************************************************************************************************************************//
+//************************************************************************************************************************//
+
+// Write a NIOS II C program that does the following:
+// 			a. Registers and enables a pushbutton interrupt for key1 
+//						b. Displays 0 on led0
+// 							c. Contains a pushbutton ISR that checks the state of SWO
+// 			i. If SW0 is high, it increments the value on hex0 
+//				ii. If SW0 is low, it decrements the value on hex0
+//************************************************************************************************************************//
+//************************************************************************************************************************//
+
 
 int main(void)
 // ****************************************************************************/
-// /* Main Program                                                              */
-// /*   Enables interrupts then loops infinitely                                */
+// /* Main Program   Part 2                                                    */
+// /*   polling to increment and decrement                                    */
 // ***************************************************************************
 {
 
 	int j = 0;
-	int startloop = 0;
 //	Set up the board and variables
 	int current_Keyval,switch_val; //will store the value of what ever is being read
 //	   what is stored in the bucket
@@ -52,9 +127,6 @@ int main(void)
 	*Hex0Ptr = array_hexDisplay[0] ;
 	//current_Keyval = *KeyPtr; //reading the current value of the key
 
-/* 		if(current_Keyval == 15)
-			{startloop = 0;}
-		else{startloop=1;} */
 
 		//while(startloop == 1){
 	while(1){
@@ -87,8 +159,11 @@ int main(void)
 
     return (0);
 }
-
-
+//************************************************************************************************************************//
+//************************************************************************************************************************//
+//************************************************************************************************************************//
+//************************************************************************************************************************//
+//************************************************************************************************************************//
 // #********This is the array that will hold the hex display values******************************************************#
 //
 // array_hexDisplay:
