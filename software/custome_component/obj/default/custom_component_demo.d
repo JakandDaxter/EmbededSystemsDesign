50917e911e570754bee24dfb5b5c51c38294c55f
JakandDaxter <aliana124@gmail.com>
2019-02-26 19:10:47 -0500

    Completed Demo

---
+++ software/custome_component/obj/default/custom_component_demo.d
@@ -0,0 +1,18 @@
+obj/default/custom_component_demo.o: custom_component_demo.c system.h \
+ ../custome_component_bsp/linker.h \
+ ../custome_component_bsp//HAL/inc/sys/alt_irq.h \
+ ../custome_component_bsp//HAL/inc/nios2.h \
+ ../custome_component_bsp//HAL/inc/alt_types.h \
+ ../custome_component_bsp/system.h
+
+system.h:
+
+../custome_component_bsp/linker.h:
+
+../custome_component_bsp//HAL/inc/sys/alt_irq.h:
+
+../custome_component_bsp//HAL/inc/nios2.h:
+
+../custome_component_bsp//HAL/inc/alt_types.h:
+
+../custome_component_bsp/system.h:
