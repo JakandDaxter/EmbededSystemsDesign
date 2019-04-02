3904275a48d38f6f7aa2bc4d94eb7086356a733b
JakandDaxter <aliana124@gmail.com>
2019-04-01 23:15:42 -0400

    update

--- hw/project/db/ip/sld859c8456/submodules/alt_sld_fab_alt_sld_fab_ident.sv
+++ hw/project/db/ip/sld859c8456/submodules/alt_sld_fab_alt_sld_fab_ident.sv
@@ -20,7 +20,7 @@
 // Altera Identification hub
 //
 // Parameters
-//   DESIGN_HASH   : 80a59a50407078e672a8
+//   DESIGN_HASH   : 50a444a96ec243a0d0db
 //   COUNT         : 1
 //   ROM_WIDTHS    : 4
 //   LATENCIES     : 0
@@ -39,7 +39,7 @@ module alt_sld_fab_alt_sld_fab_ident
 );
 
 
-wire [127:0] data_0 = { mixed, 12'h0, 32'h0, 80'h80a59a50407078e672a8 };
+wire [127:0] data_0 = { mixed, 12'h0, 32'h0, 80'h50a444a96ec243a0d0db };
 
 reg [3:0] result_0;
 always @(address_0 or data_0) begin
