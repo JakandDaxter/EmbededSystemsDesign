083c82bf889aa72708fd31c5e0ceb6483893d2ba
JakandDaxter <aliana124@gmail.com>
2019-04-17 00:13:55 -0400

    Completed demo

---
+++ Part_1/HW/software/nios_system/obj/default/Arbitration_part1.d
@@ -0,0 +1,16 @@
+obj/default/Arbitration_part1.o: Arbitration_part1.c system.h \
+ ../nios_system_bsp/linker.h ../nios_system_bsp//HAL/inc/sys/alt_irq.h \
+ ../nios_system_bsp//HAL/inc/nios2.h \
+ ../nios_system_bsp//HAL/inc/alt_types.h ../nios_system_bsp/system.h
+
+system.h:
+
+../nios_system_bsp/linker.h:
+
+../nios_system_bsp//HAL/inc/sys/alt_irq.h:
+
+../nios_system_bsp//HAL/inc/nios2.h:
+
+../nios_system_bsp//HAL/inc/alt_types.h:
+
+../nios_system_bsp/system.h:
