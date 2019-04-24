ea278a252743f072584cafaa00d6c1217f2d35f5
JakandDaxter <aliana124@gmail.com>
2019-04-24 15:44:00 -0400

    Part_2 Demoed and completed

---
+++ Part_2/HW/software/Arbitration_Nios1_APP/obj/default/main.d
@@ -0,0 +1,17 @@
+obj/default/main.o: main.c system.h ../Arbitration_Nios1_APP_bsp/linker.h \
+ ../Arbitration_Nios1_APP_bsp//HAL/inc/sys/alt_irq.h \
+ ../Arbitration_Nios1_APP_bsp//HAL/inc/nios2.h \
+ ../Arbitration_Nios1_APP_bsp//HAL/inc/alt_types.h \
+ ../Arbitration_Nios1_APP_bsp/system.h
+
+system.h:
+
+../Arbitration_Nios1_APP_bsp/linker.h:
+
+../Arbitration_Nios1_APP_bsp//HAL/inc/sys/alt_irq.h:
+
+../Arbitration_Nios1_APP_bsp//HAL/inc/nios2.h:
+
+../Arbitration_Nios1_APP_bsp//HAL/inc/alt_types.h:
+
+../Arbitration_Nios1_APP_bsp/system.h:
