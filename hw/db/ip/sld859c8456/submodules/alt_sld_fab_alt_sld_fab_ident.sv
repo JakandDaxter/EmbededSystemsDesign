b048c26d987918c16bf6671fed318ae8eca96155
JakandDaxter <aliana124@gmail.com>
2019-05-01 20:27:29 -0400

    IT JUST PASSES

--- hw/db/ip/sld859c8456/submodules/alt_sld_fab_alt_sld_fab_ident.sv
+++ hw/db/ip/sld859c8456/submodules/alt_sld_fab_alt_sld_fab_ident.sv
@@ -20,7 +20,7 @@
 // Altera Identification hub
 //
 // Parameters
-//   DESIGN_HASH   : ed0d81a6b1c7006076be
+//   DESIGN_HASH   : 44b4399f2c4d3a0851f3
 //   COUNT         : 1
 //   ROM_WIDTHS    : 4
 //   LATENCIES     : 0
@@ -39,7 +39,7 @@ module alt_sld_fab_alt_sld_fab_ident
 );
 
 
-wire [127:0] data_0 = { mixed, 12'h0, 32'h0, 80'hed0d81a6b1c7006076be };
+wire [127:0] data_0 = { mixed, 12'h0, 32'h0, 80'h44b4399f2c4d3a0851f3 };
 
 reg [3:0] result_0;
 always @(address_0 or data_0) begin
--- src/Top.vhd
+++ src/Top.vhd
@@ -9,24 +9,36 @@ entity Top is
 
 	port(		----- Clock -----
 		
-			clock  					         :in  std_logic;
+				clock  					         :in  std_logic;
 			
 				----- KEY -----
-			KEY 							 :in std_logic;
+				KEY 							 		:in std_logic;
 			
 				----- switch -----
-			SW								 :in std_logic; --connect this to selection so that i know which filter to apply
+				SW								 		:in std_logic_vector(3 downto 0); --connect this to selection so that i know which filter to apply
 				----- Adio Data Coming in -----
-			
-			data_in					    	 :in  signed(15 downto 0); --audio sample,in the 16 bit fized point format (15 bits assumed deciaml)
-			
-				----- filter enable -----
-			
-			--filter_en 				    	 :in  std_Logic; -- this enables the interal registers and coincides with a new audio sample			
-			
-				----- Output the filtered data -----
-			
-			data_out						 :out signed(15 downto 0) --this is the filtered audio signal out, in 16 bit fixed
+				DRAM_CLK,DRAM_CKE	 								: OUT STD_LOGIC;
+				DRAM_ADDR			 								: OUT STD_LOGIC_VECTOR(12 DOWNTO 0);
+				DRAM_BA           								: OUT STD_LOGIC_VECTOR(1 DOWNTO 0);
+				DRAM_CS_N,DRAM_CAS_N,DRAM_RAS_N,DRAM_WE_N	: OUT STD_LOGIC;
+				DRAM_DQ												: INOUT STD_LOGIC_VECTOR(15 DOWNTO 0);
+				DRAM_UDQM,DRAM_LDQM								: OUT STD_LOGIC;
+	
+				AUD_ADCDAT 											: IN STD_LOGIC;
+				AUD_ADCLRCK 										: INOUT STD_LOGIC;
+				AUD_BCLK 											: INOUT  STD_LOGIC;
+				AUD_DACDAT 											: OUT STD_LOGIC;
+				AUD_DACLRCK 										: INOUT  STD_LOGIC;
+				AUD_XCK                            		   : OUT STD_LOGIC;
+
+              -- the_audio_and_video_config_0
+				FPGA_I2C_SCLK	 							      : OUT STD_LOGIC;
+				FPGA_I2C_SDAT	 								   : INOUT STD_LOGIC;
+
+				
+				LEDR 													: out  std_logic_vector(9 downto 0);
+				
+				GPIO_0                             		   : INOUT STD_LOGIC_VECTOR(35 downto 0)
 			
 	    );	
 		
@@ -66,37 +78,120 @@ signal Filter_array : Low_High_Pass :=
 --------------------------                        
 -- Component Declarations
 --------------------------
-component nios_system is
-	
+	component nios_system is
 		port (
-			
-			clk_clk         : in std_logic                    := 'X';             -- clk
-			
-			reset_reset_n   : in std_logic                    := 'X';             -- reset_n
-			
-			switches_export : in std_logic 						  := 'X'  -- export
-		
+			AUD_ADCDAT_to_the_audio_0   : in    std_logic                     := 'X';             -- ADCDAT
+			AUD_ADCLRCK_to_the_audio_0  : in    std_logic                     := 'X';             -- ADCLRCK
+			AUD_BCLK_to_the_audio_0     : in    std_logic                     := 'X';             -- BCLK
+			AUD_DACDAT_from_the_audio_0 : out   std_logic;                                        -- DACDAT
+			AUD_DACLRCK_to_the_audio_0  : in    std_logic                     := 'X';             -- DACLRCK
+			clk_clk                     : in    std_logic                     := 'X';             -- clk
+			i2c_SDAT                    : inout std_logic                     := 'X';             -- SDAT
+			i2c_SCLK                    : out   std_logic;                                        -- SCLK
+			pin_export                  : out   std_logic;                                        -- export
+			reset_reset                 : in    std_logic                     := 'X';             -- reset
+			sdram_addr                  : out   std_logic_vector(12 downto 0);                    -- addr
+			sdram_ba                    : out   std_logic_vector(1 downto 0);                     -- ba
+			sdram_cas_n                 : out   std_logic;                                        -- cas_n
+			sdram_cke                   : out   std_logic;                                        -- cke
+			sdram_cs_n                  : out   std_logic;                                        -- cs_n
+			sdram_dq                    : inout std_logic_vector(15 downto 0) := (others => 'X'); -- dq
+			sdram_dqm                   : out   std_logic_vector(1 downto 0);                     -- dqm
+			sdram_ras_n                 : out   std_logic;                                        -- ras_n
+			sdram_we_n                  : out   std_logic;                                        -- we_n
+			sdram_clk_clk               : out   std_logic;                                        -- clk
+			sw_export                   : in    std_logic_vector(3 downto 0)  := (others => 'X')  -- export
 		);
-	
 	end component nios_system;
 
 --------------------------                        
 -- Signal Declarations
 --------------------------
-    
-signal Input_data 			 : signed(15 downto 0)   := (others => '0');
 
-signal output_data 			 : signed(15 downto 0)   := (others => '0'); 
+signal test_sig        					: std_logic;
+    
+signal Input_data 			 			: signed(15 downto 0)   := (others => '0');
+
+signal output_data 			 			: signed(15 downto 0)   := (others => '0');
+
+signal DRAM_DQM 							: std_logic_vector(1 DOWNTO 0);
+signal int_AUD_BCLK  					: std_logic;
+signal int_AUD_DACDAT  					: std_logic;
+signal int_AUD_DACLRCK 					: std_logic; 
  
-signal reset_n 				 : std_logic;
-signal key0_d1 				 : std_logic;
-signal key0_d2 				 : std_logic;
-signal key0_d3 				 : std_logic;
+signal reset_n 				 			: std_logic;
+signal key0_d1 				 			: std_logic;
+signal key0_d2 				 			: std_logic;
+signal key0_d3 				 			: std_logic;
+
+signal count           					: std_logic_vector(3 downto 0);
 
 -----------------------------------------------------			  
 -----------------------------------------------------																	
 
 BEGIN
+
+
+
+	clkgen: process(clock, reset_n) 
+	
+		
+		begin
+			
+			if (reset_n = '0') then
+				
+				LEDR <= "1111111111";
+        
+		  elsif (rising_edge(clock)) then
+           
+			  LEDR <= "0011001100";
+			
+			end if;
+		
+		end process;
+
+-----------------------------------------------------			  		  
+  ----- Audio Demo Code
+-----------------------------------------------------   
+   DRAM_UDQM <= DRAM_DQM(1);
+	
+	
+	DRAM_LDQM <= DRAM_DQM(0);
+   
+	--int_AUD_BCLK <= AUD_BCLK;
+ 	
+	GPIO_0(0) <= AUD_BCLK; 
+	
+	AUD_DACDAT <= int_AUD_DACDAT;
+	
+	GPIO_0(1) <= int_AUD_DACDAT;
+   
+	--int_AUD_DACLRCK <= AUD_DACLRCK;
+ 	
+	GPIO_0(2) <= AUD_DACLRCK;   
+	
+	GPIO_0(3) <= test_sig;
+	
+	AUD_XCK <= count(1);	
+-----------------------------------------------------			  		  
+  ----- some clock process
+----------------------------------------------------- 
+	LED_Process: process(clock, reset_n) 
+	
+		
+		begin
+			
+			if (reset_n = '0') then
+				
+				count <= "0000";
+        
+		  elsif (rising_edge(clock)) then
+           
+			  count <= count + 1;
+			
+			end if;
+		
+		end process;
  
 -----------------------------------------------------			  		  
   ----- Syncronize the reset
@@ -121,16 +216,29 @@ BEGIN
 --------------------------                        
 -- Port Map nios system
 --------------------------
-u0 : component nios_system
-		
+	u0 : component nios_system
 		port map (
-		
-			clk_clk         => clock,         --      clk.clk
-			
-			reset_reset_n   => reset_n,   --    reset.reset_n
-			
-			switches_export => SW  -- switches.export
-		
+			AUD_ADCDAT_to_the_audio_0   => AUD_ADCDAT,   					--     audio.ADCDAT
+			AUD_ADCLRCK_to_the_audio_0  => AUD_ADCLRCK,  					--          .ADCLRCK
+			AUD_BCLK_to_the_audio_0     => AUD_BCLK,     					--          .BCLK
+			AUD_DACDAT_from_the_audio_0 => int_AUD_DACDAT,						 --          .DACDAT
+			AUD_DACLRCK_to_the_audio_0  => AUD_DACLRCK, 						 --          .DACLRCK
+			clk_clk                     => clock,                     --       clk.clk
+			i2c_SDAT                    => FPGA_I2C_SDAT,                    --       i2c.SDAT
+			i2c_SCLK                    => FPGA_I2C_SCLK,                    --          .SCLK
+			pin_export                  => test_sig,                  --       pin.export
+			reset_reset                 => not reset_n,                 --     reset.reset
+			sdram_addr                  => DRAM_ADDR,                  --     sdram.addr
+			sdram_ba                    => DRAM_BA,                    --          .ba
+			sdram_cas_n                 => DRAM_CAS_N,                 --          .cas_n
+			sdram_cke                   => DRAM_CKE,                   --          .cke
+			sdram_cs_n                  => DRAM_CS_N,                  --          .cs_n
+			sdram_dq                    => DRAM_DQ,                    --          .dq
+			sdram_dqm                   => DRAM_DQM,                   --          .dqm
+			sdram_ras_n                 => DRAM_RAS_N,                 --          .ras_n
+			sdram_we_n                  => DRAM_WE_N,                  --          .we_n
+			sdram_clk_clk               => DRAM_CLK,               -- sdram_clk.clk
+			sw_export                   => SW                    --        sw.export
 		);
     
 end lowandHigh_pass_filter;
